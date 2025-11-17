ed69ec0d013cd656dcaa90bdb396d5d18a593d62
Azuri Caelum <arokima@gmail.com>
2025-11-17 13:56:12 -0600

    unfinished

--- worlds/ourascent/regions.py
+++ worlds/ourascent/regions.py
@@ -1,6 +1,6 @@
 from typing import TYPE_CHECKING, Dict, List
 
-from BaseClasses import Entrance, Region
+from BaseClasses import Region
 from .locations import LocationData
 from .rules import apolonia_offense
 
@@ -14,41 +14,41 @@ def create_and_connect_regions(world: OurAscentWorld) -> None:
 
 def create_all_regions(world: OurAscentWorld) -> None:
     story_select = Region("Story Select", world.player, world.multiworld)
-    APOLONIA_STAGE_0 = Region("1-1 Beginning-Right", world.player, world.multiworld)
-    APOLONIA_STAGE_1 = Region("1-1 Beginning-Left", world.player, world.multiworld)
-    APOLONIA_STAGE_2 = Region("1-1 Further Right", world.player, world.multiworld)
-    APOLONIA_STAGE_3 = Region("1-1 Further Left", world.player, world.multiworld)
-    APOLONIA_STAGE_4 = Region("1-1 Furthest Right", world.player, world.multiworld)
-    APOLONIA_STAGE_5 = Region("1-1 Furthest Left", world.player, world.multiworld)
-    APOLONIA_STAGE_6 = Region("1-1 Center", world.player, world.multiworld)
-    APOLONIA_STAGE_7 = Region("1-1 Center Continued", world.player, world.multiworld)
-    APOLONIA_STAGE_8 = Region("1-1 Endgame", world.player, world.multiworld)
+    STAGE_11_REGION_0 = Region("1-1 Beginning-Right", world.player, world.multiworld)
+    STAGE_11_REGION_1 = Region("1-1 Beginning-Left", world.player, world.multiworld)
+    STAGE_11_REGION_2 = Region("1-1 Further Right", world.player, world.multiworld)
+    STAGE_11_REGION_3 = Region("1-1 Further Left", world.player, world.multiworld)
+    STAGE_11_REGION_4 = Region("1-1 Furthest Right", world.player, world.multiworld)
+    STAGE_11_REGION_5 = Region("1-1 Furthest Left", world.player, world.multiworld)
+    STAGE_11_REGION_6 = Region("1-1 Center", world.player, world.multiworld)
+    STAGE_11_REGION_7 = Region("1-1 Center Continued", world.player, world.multiworld)
+    STAGE_11_REGION_8 = Region("1-1 Endgame", world.player, world.multiworld)
 
-    regions = [story_select, APOLONIA_STAGE_0, APOLONIA_STAGE_1, APOLONIA_STAGE_2, APOLONIA_STAGE_3, APOLONIA_STAGE_4, APOLONIA_STAGE_5, APOLONIA_STAGE_6, APOLONIA_STAGE_7, APOLONIA_STAGE_8]
+    regions = [story_select, STAGE_11_REGION_0, STAGE_11_REGION_1, STAGE_11_REGION_2, STAGE_11_REGION_3, STAGE_11_REGION_4, STAGE_11_REGION_5, STAGE_11_REGION_6, STAGE_11_REGION_7, STAGE_11_REGION_8]
 
 def connect_regions(world: OurAscentWorld) -> None:
     story_select = world.get_region("Main Menu")
-    APOLONIA_STAGE_0 = world.get_region("1-1 Beginning-Right")
-    APOLONIA_STAGE_1 = world.get_region("1-1 Beginning-Left")
-    APOLONIA_STAGE_2 = world.get_region("1-1 Further Right")
-    APOLONIA_STAGE_3 = world.get_region("1-1 Further Left")
-    APOLONIA_STAGE_4 = world.get_region("1-1 Furthest Right")
-    APOLONIA_STAGE_5 = world.get_region("1-1 Furthest Left")
-    APOLONIA_STAGE_6 = world.get_region("1-1 Center")
-    APOLONIA_STAGE_7 = world.get_region("1-1 Center Continued")
-    APOLONIA_STAGE_8 = world.get_region("1-1 Endgame")
+    STAGE_11_REGION_0 = world.get_region("1-1 Beginning-Right")
+    STAGE_11_REGION_1 = world.get_region("1-1 Beginning-Left")
+    STAGE_11_REGION_2 = world.get_region("1-1 Further Right")
+    STAGE_11_REGION_3 = world.get_region("1-1 Further Left")
+    STAGE_11_REGION_4 = world.get_region("1-1 Furthest Right")
+    STAGE_11_REGION_5 = world.get_region("1-1 Furthest Left")
+    STAGE_11_REGION_6 = world.get_region("1-1 Center")
+    STAGE_11_REGION_7 = world.get_region("1-1 Center Continued")
+    STAGE_11_REGION_8 = world.get_region("1-1 Endgame")
 
     #Story 1-1
-    story_select.connect(APOLONIA_STAGE_0, "Begin story 1-1 to level 100 boss", lambda state: state.has("Character - Playable Apolonia", world.player, 1))
-    APOLONIA_STAGE_0.connect(APOLONIA_STAGE_1, "Take left path to level 200 boss",  lambda state: state.has(apolonia_offense, world.player, 5))
-    APOLONIA_STAGE_0.connect(APOLONIA_STAGE_2, "Continue right path to level 700 boss", lambda state: state.has(apolonia_offense, world.player, 13))
-    APOLONIA_STAGE_1.connect(APOLONIA_STAGE_3, "Continue left path to level 500 boss", lambda state: state.has(apolonia_offense, world.player, 12))
-    APOLONIA_STAGE_2.connect(APOLONIA_STAGE_4, "Continue right path to level 1200 boss", lambda state: state.has(apolonia_offense, world.player, 20))
-    APOLONIA_STAGE_3.connect(APOLONIA_STAGE_5, "Continue left path to level 1300 boss", lambda state: state.has(apolonia_offense, world.player, 19))
-    APOLONIA_STAGE_4.connect(APOLONIA_STAGE_6, "Take right path to level 3000 boss", lambda state: state.has(apolonia_offense, world.player, 30))
-    APOLONIA_STAGE_5.connect(APOLONIA_STAGE_6, "Take left path to level 3000 boss", lambda state: state.has(apolonia_offense, world.player, 30))
-    APOLONIA_STAGE_6.connect(APOLONIA_STAGE_7, "Continue central area to level 5000 boss", lambda state: state.has(apolonia_offense, world.player, 40))
-    APOLONIA_STAGE_7.connect(APOLONIA_STAGE_8, "Go north from central area to endgame", lambda state: state.has(apolonia_offense, world.player, 48))
+    story_select.connect(STAGE_11_REGION_0, "Begin story 1-1 to level 100 boss", lambda state: state.has("Character - Playable Apolonia", world.player, 1))
+    STAGE_11_REGION_0.connect(STAGE_11_REGION_1, "Take left path to level 200 boss",  lambda state: state.has(apolonia_offense, world.player, 5))
+    STAGE_11_REGION_0.connect(STAGE_11_REGION_2, "Continue right path to level 700 boss", lambda state: state.has(apolonia_offense, world.player, 13))
+    STAGE_11_REGION_1.connect(STAGE_11_REGION_3, "Continue left path to level 500 boss", lambda state: state.has(apolonia_offense, world.player, 12))
+    STAGE_11_REGION_2.connect(STAGE_11_REGION_4, "Continue right path to level 1200 boss", lambda state: state.has(apolonia_offense, world.player, 20))
+    STAGE_11_REGION_3.connect(STAGE_11_REGION_5, "Continue left path to level 1300 boss", lambda state: state.has(apolonia_offense, world.player, 19))
+    STAGE_11_REGION_4.connect(STAGE_11_REGION_6, "Take right path to level 3000 boss", lambda state: state.has(apolonia_offense, world.player, 30))
+    STAGE_11_REGION_5.connect(STAGE_11_REGION_6, "Take left path to level 3000 boss", lambda state: state.has(apolonia_offense, world.player, 30))
+    STAGE_11_REGION_6.connect(STAGE_11_REGION_7, "Continue central area to level 5000 boss", lambda state: state.has(apolonia_offense, world.player, 40))
+    STAGE_11_REGION_7.connect(STAGE_11_REGION_8, "Go north from central area to endgame", lambda state: state.has(apolonia_offense, world.player, 48))
 
 def get_locations_per_ap_region(locations: List[LocationData]) -> Dict[str, List[LocationData]]:
     per_region: Dict[str, List[LocationData]] = {}
--- worlds/ourascent/world.py
+++ worlds/ourascent/world.py
@@ -9,81 +9,95 @@ from .locations import get_locations
 from .options import OurAscentOptions
 from .stories import story_id_to_name
 
+
 class OurAscentWorld(World):
-  """Our Ascent is an incremental roguelite RPG with many layers of metaprogression."""
-  game = "Our Ascent"
-  web = web_world.OurAscentWebWorld()
+    """Our Ascent is an incremental roguelite RPG with many layers of metaprogression."""
+    game = "Our Ascent"
+    web = web_world.OurAscentWebWorld()
 
-  options_dataclass = options.OurAscentOptions
-  options: options.OurAscentOptions
+    options_dataclass = options.OurAscentOptions
+    options: options.OurAscentOptions
 
-  item_name_to_id = {item: item_table[item].code for item in item_table}
-  location_name_to_id = {location.name: location.code for location in get_locations(-1, options)}
-  playable_stories = [value for _, value in story_id_to_name.items()]
-  starting_story = "1-1: Falling Into Chaos"
+    item_name_to_id = {item: item_table[item].code for item in item_table}
+    location_name_to_id = {location.name: location.code for location in get_locations(-1, options)}
+    playable_stories = [value for _, value in story_id_to_name.items()]
+    starting_story = "1-1: Falling Into Chaos"
 
-  origin_region_name = "Story Select"
+    origin_region_name = "Story Select"
 
-  def create_regions(self) -> None:
-      regions.create_and_connect_regions(self)
-      locations.create_all_locations(self)
+    def create_regions(self) -> None:
+        regions.create_and_connect_regions(self)
+        locations.create_all_locations(self)
 
-  def set_rules(self) -> None:
-      rules.set_all_rules(self)
+    def set_rules(self) -> None:
+        rules.set_all_rules(self)
 
-  def create_items(self) -> None:
-      pool = self.get_all_items()
-      self.multiworld.itempool += pool
+    def create_items(self) -> None:
+        pool = self.get_all_items()
+        self.multiworld.itempool += pool
 
-  def create_item(self, name: str) -> Item:
-      data = item_table[name]
-      return Item(name, data.classification, data.code, self.player)
+    def create_item(self, name: str) -> Item:
+        data = item_table[name]
+        return Item(name, data.classification, data.code, self.player)
 
-  def get_filler_item_name(self) -> str:
-      return items.get_random_filler_item_name(self)
+    def get_filler_item_name(self) -> str:
+        return items.get_random_filler_item_name(self)
 
-  def fill_slot_data(self) -> Mapping[str, Any]:
-      return self.options.as_dict()
+    def fill_slot_data(self) -> Mapping[str, Any]:
+        return self.options.as_dict()
 
-  def get_all_items(self, excluded_items: Set[str]) -> List[Item]:
-      pool: List[Item] = []
-      self.get_stories()
-      amount:int = int(0)
-      for name, data in item_table.items():
-          for _ in range(amount):
-              item = self.set_classifications(name)
-              pool.append(item)
-      return pool
+    def get_all_items(self, excluded_items: Set[str]) -> List[Item]:
+        pool: List[Item] = []
+        self.get_stories()
+        amount: int = int(0)
+        for name, data in item_table.items():
+            for _ in range(amount):
+                item = self.set_classifications(name)
+                pool.append(item)
+        return pool
 
-#  def generate_early(self):
-  def get_stories(self):
-      playable_story_choice = list(
-          [value for key, value in story_id_to_name.items()])
-      if self.options.last_chapter == 1:
+    #  def generate_early(self):
+    def get_stories(self):
+        #Need the list of potential stories from the latest chapter indicated
+        playable_story_choice = list(
+            [value for key, value in story_id_to_name.items()])
+        if self.options.last_chapter == 1:
+            story_range = 1
+            while story_range < 6:
+                playable_story_choice.append(range)
+        if self.options.last_chapter == 2:
+            story_range = 7
+            while story_range < 9:
+                playable_story_choice.append(range)
+
+        #Get the list of latest chapter stories to use in the world
+        self.random.shuffle(playable_story_choice)
+        self.playable_stories = playable_story_choice[0:
+                                                      self.options.story_count.value]
+
+        #If a story from a later chapter is in the multiworld, it needs its requisite story(ies) from the previous chapter
+        starting_story_pool = self.playable_stories[1]
+        if 7 in self.playable_stories:
+            self.playable_stories += 1
+            self.playable_stories += 2
+        if 8 in self.playable_stories:
+            self.playable_stories += 3
+            self.playable_stories += 5
+
+        #Get the Glades chapters
         story_range = 1
-        while story_range < 6:
-            playable_story_choice.append(range)
-      if self.options.last_chapter == 2:
-        story_range = 7
-        while story_range < 9:
-            playable_story_choice.append(range)
+        while story_range < 7:
+            if story_range in self.playable_stories:
+                starting_story_pool.append(story_range)
 
-      self.random.shuffle(playable_story_choice)
-      self.playable_stories = playable_story_choice[0:
-                                                 self.options.story_count.value]
+        #Randomize the starting chapter, it must be Glades for the natural progression to work
+        story = self.random.choice(starting_story_pool)
+        self.starting_story = story
 
-      starting_story_pool = self.playable_stories[1]
-      if 7 in self.playable_stories:
-        self.playable_stories += 1
-        self.playable_stories += 2
-      if 8 in self.playable_stories:
-        self.playable_stories += 3
-        self.playable_stories += 5
-
-      character_table: Dict[str, ItemData] = {}
-      for item in item_table:
-        if is_character(item) and (item in self.playable_stories):
-            character_table[item] = item_table[item]
+        character_table: Dict[str, ItemData] = {}
+        for item in item_table:
+            if is_character(item) and (item in self.playable_stories):
+                character_table[item] = item_table[item]
 
         character_table = list(character_table.items())
         character = self.random.choice(character_table)
